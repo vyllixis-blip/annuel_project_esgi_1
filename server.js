21fb2fae229e3e83017201d067c803676b62d3df
Théo Moitrier-Cabodi <vyllixis@gmail.com>
2026-04-28 14:15:18 +0200

    fix(website) ajout des dernier fichier

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const path = require('node:path')
+
+const PORT = 4000
+
+app.use(cors())
+app.use(express.static(path.join(__dirname, '')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
